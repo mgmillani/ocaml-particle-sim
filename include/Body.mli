940c7d65ab0acd95b14d8c117224637f863e799e
Marcelo Garlet Millani <marcelogmillani@gmail.com>
2013-06-14 19:07:13 -0300

    Relatório feito

--- include/Body.mli
+++ include/Body.mli
@@ -5,7 +5,10 @@ class virtual body:
 		val mutable position : float * float
 		val mutable velocity : float * float
 		val mutable acceleration : float * float
+		method getPosition : float*float
+		method setPosition : float*float -> unit
 		method getAcceleration : float*float
+		method loopBound : float*float -> float*float -> unit
 		method setAcceleration : float*float-> unit
 		method applyAcceleration : float*float -> unit
 		method virtual draw : unit
